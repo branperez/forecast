88fbe25694ffe1bad3a4a1c7cdc4d2b3e02ef615
brandoff <bperez83525@gmail.com>
2018-07-27 16:18:39 -0500

    deployable war

--- src/main/java/me/brandonmichael/forcast/ForecastApplication.java
+++ src/main/java/me/brandonmichael/forcast/ForecastApplication.java
@@ -2,9 +2,16 @@ package me.brandonmichael.forcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ForecastApplication {
+public class ForecastApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ForecastApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForecastApplication.class, args);
